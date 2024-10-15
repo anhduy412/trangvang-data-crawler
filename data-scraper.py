3a30e776cdc57daa968e81a54abc5b49b622647b
Duy Tran <anhduytran5454@gmail.com>
2024-10-16 00:05:19 +0700

    modify some lines to optimise

--- data-scraper.py
+++ data-scraper.py
@@ -12,7 +12,7 @@ company_info = []
 for page in range(1, 3):
     # Construct the full URL for the current page
     url = f"{base_url}{page}"
-    
+
     # Send a request to the website
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -24,31 +24,26 @@ for page in range(1, 3):
     for company in companies:
         name = company.find('h2', class_="p-1 fs-5 h2 m-0 pt-0 ps-0 text-capitalize").get_text(strip=True)
 
-        # Extract address
-        address_div = company.find('div', class_='logo_congty_diachi')
-        if address_div:
+        if address_div := company.find('div', class_='logo_congty_diachi'):
+            address = address_div.find_all('div')[1].get_text(strip=True) if len(address_div.find_all('div')) > 1 else ''
+        elif address_div := company.find(
+                'div', class_='listing_diachi_nologo'
+            ):
             address = address_div.find_all('div')[1].get_text(strip=True) if len(address_div.find_all('div')) > 1 else ''
         else:
-            address_div = company.find('div', class_='listing_diachi_nologo')
-            if address_div:
-                address = address_div.find_all('div')[1].get_text(strip=True) if len(address_div.find_all('div')) > 1 else ''
-            else:
-                address = ''
+            address = ''
 
-        # Extract phone number
-        phone_div = company.find('div', class_='listing_dienthoai')
-        if phone_div:
+        if phone_div := company.find('div', class_='listing_dienthoai'):
+            phone = phone_div.find('a').get_text(strip=True) if phone_div.find('a') else ''
+        elif phone_div := company.find(
+                'div', class_='p-2 pt-0 ps-0 pe-4 pb-0'
+            ):
             phone = phone_div.find('a').get_text(strip=True) if phone_div.find('a') else ''
         else:
-            # If not found, check the second div
-            phone_div = company.find('div', class_='p-2 pt-0 ps-0 pe-4 pb-0')
-            if phone_div:
-                phone = phone_div.find('a').get_text(strip=True) if phone_div.find('a') else ''
-            else:
-                phone = ''
+            phone = ''
 
         email_section = company.find('div', class_='email_web_section')
-        
+
         # Initialize email and website to ''
         email = ''
         website = ''
@@ -57,11 +52,11 @@ for page in range(1, 3):
         if email_section:
             email_link = email_section.find('a', href=lambda href: href and 'mailto:' in href)
             website_link = email_section.find('a', href=lambda href: href and 'http' in href)
-            
+
             # Extract email if available
             if email_link:
                 email = email_link.get_text(strip=True)
-            
+
             # Extract website if available
             if website_link:
                 website = website_link.get_text(strip=True)
