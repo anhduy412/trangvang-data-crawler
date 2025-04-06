6316e4a5cd3764b84815dfcf1e04c531267e9f68
Duy Tran <anhduytran5454@gmail.com>
2025-04-06 23:37:55 +0700

    add something

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ import config
 # Crawl company info
 company_info = crawl_companies_info(config.url_list)
 save_to_csv(company_info, config.file_name)
-print(f'Done, check {config.file_name} for data.')
+print(f'Done, please check {config.file_name} for data.')
